928135ee57fe6816759219b56216be3e7bb7f22f
amao3 <amao@bu.edu>
2025-06-15 21:11:30 -0400

    fixed the chat template using llm.chat() instead of llm.generate()

--- src/tot/models_deepseek_vllm.py
+++ src/tot/models_deepseek_vllm.py
@@ -6,7 +6,7 @@ def deepseek(eval_prompt_list, model='/stage/hf_cache/DeepSeek-V2-Lite-Chat', te
     sampling_params = SamplingParams(temperature=temperature,max_tokens=max_tokens, n=n, seed=seed_value)
     # Create an LLM. Load model weights to GPU.
     llm = LLM(model=model, trust_remote_code=True, max_model_len=2048)
-    outputs = llm.generate(eval_prompt_list, sampling_params)
+    outputs = llm.chat(eval_prompt_list, sampling_params)
     responses = []
     for output in outputs:
         responses.append([output.outputs[i].text for i in range(len(output.outputs))])
